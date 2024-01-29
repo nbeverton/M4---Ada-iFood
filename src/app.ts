6861dca6a20be365b7e3765cb108f4bb5e465ef0
Everton Barbosa <104376567+nbeverton@users.noreply.github.com>
2024-01-29 19:53:39 -0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,51 @@
+import express, { NextFunction, Request, Response } from 'express'
+import path from 'path'
+import { logCadVeiculosMiddleware } from './middleware/logCadVeiculos'
+import { validarDadosEntrada } from './middleware/validacao';
+import { VeiculoController } from './controllers/veiculoController';
+import { ClienteController } from './controllers/clienteController';
+import { AluguelController } from './controllers/aluguelController';
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/veiculos', (req: Request, res: Response, next: NextFunction) => {
+    const veiculosDisponiveis = new VeiculoController().buscarVeiculos();
+    res.status(200).send(veiculosDisponiveis);
+    return next();
+})
+
+app.post('/veiculos', validarDadosEntrada, logCadVeiculosMiddleware.execute, (req: Request, res: Response, next: NextFunction) => {
+    const veiculoCriado = new VeiculoController().adicionarVeiculo(req);
+    res.status(201).send(veiculoCriado);
+    return next();
+})
+
+app.get('/clientes', (req: Request, res: Response, next: NextFunction) => {
+    const clientes = new ClienteController().listarClientes();
+    res.status(200).send(clientes);
+    return next();
+})
+
+app.post('/clientes', (req: Request, res: Response, next: NextFunction) => {
+    const clienteCriado = new ClienteController().adicionarCliente(req)
+    res.status(201).send(clienteCriado);
+    return next();
+})
+
+app.get('/alugueis', (req: Request, res: Response, next: NextFunction) => {
+    const alugueis = new AluguelController().listarAlugueis();
+    res.status(200).send(alugueis);
+})
+
+app.post('/alugueis', (req: Request, res: Response, next: NextFunction) => {
+    const aluguelCriado = new AluguelController().alugarVeiculo(req);
+    res.status(201).send(aluguelCriado);
+})
+
+
+
+app.listen(3000, () => {
+    console.log('Servidor iniciado...');
+})
---
+++ src/controllers/veiculoController.ts
@@ -0,0 +1,26 @@
+import { Request } from 'express';
+import { Veiculo } from '../models/veiculo';
+import { VeiculoService } from '../services/veiculoService';
+
+export class VeiculoController {
+
+    private veiculoService: VeiculoService;
+
+    constructor() {
+        this.veiculoService = new VeiculoService();
+    }
+    buscarVeiculos() {
+        return this.veiculoService.buscarVeiculos()
+    }
+
+    adicionarVeiculo(req: Request) {
+        const novoVeiculo = new Veiculo(req.body);
+
+        return this.veiculoService.adicionarVeiculo(novoVeiculo);
+    }
+
+    listarVeiculosDisponiveis() {
+        return this.veiculoService.listarVeiculosDisponiveis();
+    }
+
+}
